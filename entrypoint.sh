f334a483bd050063be23b17b19c6478a231411c0
Teagan glenn <teagan.m.glenn@gmail.com>
2023-07-15 22:39:30 -0600

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-pip3 install flask==2.3.2
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-python3 api.py
+apt-get update && apt-get install -y python3 python3-dev python3-pip
+
+pip install flask[async]==2.3.2 pyyaml==6.0.0
+
+python3 "$SCRIPT_DIR/api.py"
